57a42b200926b07601253e3bb7a104cafa30c8ca
beuleu <billyklotz789@gmail.com>
2022-12-18 14:06:43 +0100

    Cover w/ transition DONE

---
+++ fuckCover.js
@@ -0,0 +1,6 @@
+const fuckCoverButton = document.querySelector(".fuck-cover-btn")
+const cover = document.querySelector(".cover")
+
+fuckCoverButton.addEventListener("click", () => {
+    cover.classList.add("fuck-cover")
+})
