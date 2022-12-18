7054011cbfe3b866d1661b8a278017c24e6edd35
beuleu <billyklotz789@gmail.com>
2022-12-18 21:41:03 +0100

    Tech Stack DONE

--- mobileMenu.js
+++ mobileMenu.js
@@ -3,14 +3,17 @@ const menu = document.querySelector(".left")
 noMenu = document.querySelector(".logo-simplon")
 contactItems = document.querySelectorAll(".contact ul li")
 
+
 toggleBtn.addEventListener("click", () => {
     menu.classList.add("mobile-menu")
+    noMenu.style.alignSelf = "flex-start"
     contactItems.forEach(item => {
         item.classList.add("menu-mobile-contact")
     })
 })
 noMenu.addEventListener("click", () => {
     menu.classList.remove("mobile-menu")
+    noMenu.style.alignSelf = "flex-end"
     contactItems.forEach(item => {
         item.classList.remove("menu-mobile-contact")
     })
