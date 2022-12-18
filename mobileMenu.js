da98b7708c1d05e6b68c0969d66728238b462fe8
beuleu <billyklotz789@gmail.com>
2022-12-18 20:27:21 +0100

    Contact DONE

--- mobileMenu.js
+++ mobileMenu.js
@@ -1,10 +1,17 @@
 const toggleBtn = document.querySelector(".extend-cv")
 const menu = document.querySelector(".left")
 noMenu = document.querySelector(".logo-simplon")
+contactItems = document.querySelectorAll(".contact ul li")
 
 toggleBtn.addEventListener("click", () => {
     menu.classList.add("mobile-menu")
+    contactItems.forEach(item => {
+        item.classList.add("menu-mobile-contact")
+    })
 })
 noMenu.addEventListener("click", () => {
     menu.classList.remove("mobile-menu")
+    contactItems.forEach(item => {
+        item.classList.remove("menu-mobile-contact")
+    })
 })
